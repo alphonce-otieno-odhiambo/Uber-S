ba179cf0e38a144f5e32a61e0809e606e3e985a9
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-04-24 14:55:24 +0300

    install redux

--- uber-s/App.js
+++ uber-s/App.js
@@ -1,14 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
 
 //set up redux
 
 export default function App() {
   return (
+    <Provider store={}>
     <View style={styles.container}>
       <Text>Buda lamba</Text>
       <StatusBar style="auto" />
     </View>
+    </Provider>
   );
 }
 
