ed9d3282a7bb291a35b6a12c12c7e705f2a50f86
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-04-24 16:09:39 +0300

    create homeScreen

--- uber-s/App.js
+++ uber-s/App.js
@@ -1,16 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
+import { store } from './store';
 
 //set up redux
 
 export default function App() {
   return (
-    <Provider store={}>
-    <View style={styles.container}>
-      <Text>Buda lamba</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <HomeScreen/>
     </Provider>
   );
 }
